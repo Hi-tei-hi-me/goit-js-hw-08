1ea12a4176e92f94ee61016514420a83dbce888e
Hi-tei-hi-me <lethargium@gmail.com>
2022-09-25 16:12:22 +0300

    Task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryMarkup = galleryItems.map(picture => {
+  return /*html*/ `<li><a class="gallery__item" href="${picture.original}" onclick="event.preventDefault()">
+    <img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+  </a></li>`;
+});
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup.join(''));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
